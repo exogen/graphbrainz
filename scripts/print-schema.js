d67e6433b4d08ffbd68ffe5b095ef3dff1eeeffb
Brian Beck <exogen@gmail.com>
2016-08-30 18:39:28 -0700

    Add support for printing JSON schema, generate on build

--- scripts/print-schema.js
+++ scripts/print-schema.js
@@ -1,4 +1,14 @@
-import { printSchema } from 'graphql'
+import { graphql, introspectionQuery, printSchema } from 'graphql'
 import schema from '../src/schema'
 
-console.log(printSchema(schema))
+if (require.main === module) {
+  if (process.argv[2] === '--json') {
+    graphql(schema, introspectionQuery).then(result => {
+      console.log(JSON.stringify(result, null, 2))
+    }).catch(err => {
+      console.error(err)
+    })
+  } else {
+    console.log(printSchema(schema))
+  }
+}
