e0204281291acb08acfa82cd507817ea18835904
Brian Beck <exogen@gmail.com>
2017-11-17 20:50:27 -0800

    Fix resolveType after the schema has been extended

--- src/types/entity.js
+++ src/types/entity.js
@@ -4,9 +4,19 @@ import { mbid, connectionWithExtras } from './helpers'
 const Entity = new GraphQLInterfaceType({
   name: 'Entity',
   description: 'An entity in the MusicBrainz schema.',
-  resolveType(value) {
-    if (value._type && require.resolve(`./${value._type}`)) {
-      return require(`./${value._type}`).default
+  resolveType(value, context, info) {
+    if (value._type) {
+      const typeMap = info.schema.getTypeMap()
+      let originalType
+      try {
+        originalType = require(`./${value._type}`).default
+      } catch (err) {
+        return
+      }
+      // Don't use `originalType`! The schema may have been extended in which
+      // case the types have all been replaced. Instead, find the current type
+      // of the same name.
+      return typeMap[originalType.name]
     }
   },
   fields: () => ({ mbid })
